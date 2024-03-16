c2542563e81fc0621a867f0363f723661bbd3090
jessecaps <jcaps@umich.edu>
2024-03-15 22:35:05 -0400

    Update dump_ensight.f90
    Dump object index to EnSight if using IBM

--- src/postprocess/dump_ensight.f90
+++ src/postprocess/dump_ensight.f90
@@ -298,6 +298,10 @@ contains
           ! Levelset
           str='scalar per node: 1 LEVELSET LEVELSET/LEVELSET.******'
           write(iunit,'(a80)') str
+
+          ! Object index
+          str='scalar per node: 1 OBJ_ID OBJ_ID/OBJ_ID.******'
+          write(iunit,'(a80)') str
           
           if (ibm_move) then
              str='vector per node: 1 IBM_VEL IBM_VEL/IBM_VEL.******'
@@ -1824,6 +1828,10 @@ subroutine dump_ensight_data(mode)
         ! Levelset
         name = 'LEVELSET'
         call dump_ensight_scalar(name, levelset(:,1))
+
+        ! Object index
+        name = 'OBJ_ID'
+        call dump_ensight_scalar(name, real(objectIndex,WP))
 
         if (ibm_move) then
            name = 'IBM_VEL'
